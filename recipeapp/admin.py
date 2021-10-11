1d0898e6b02da081c90ca50e2ca6e5765cc57cfe
hartnetl <laura.hartnett@outlook.com>
2021-10-11 12:58:54 +0000

    Feat: Add filter and search options to admin panel

--- recipeapp/admin.py
+++ recipeapp/admin.py
@@ -13,5 +13,9 @@ class CategoryInline(admin.TabularInline):
 
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin, admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'date_created', 'approval', 'categories__name')
+    search_fields = ['title', 'about', 'method', 'ingredients__item', 'categories__name']
+    list_display = ('title', 'slug', 'status', 'date_created')
     summernote_fields = ('about', 'method', 'nutrition')
     inlines = [IngredientInline, CategoryInline, ]
