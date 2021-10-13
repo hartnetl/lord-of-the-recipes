c340f7db68a9c6c07296e3bf63dab3e1980f5476
hartnetl <laura.hartnett@outlook.com>
2021-10-13 23:02:29 +0000

    Feat: Link up navbar urls

--- recipeapp/models.py
+++ recipeapp/models.py
@@ -67,3 +67,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.message} by {self.name}"
+
--- recipeapp/urls.py
+++ recipeapp/urls.py
@@ -6,9 +6,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('recipe/', views.RecipeList.as_view(), name='recipes'),
     path('<slug:slug>/', views.FullRecipe.as_view(), name='full_recipe'),
-    path('', TemplateView.as_view(template_name="index.html")),
-    path('home', TemplateView.as_view(template_name="index.html")),
-    path('about', TemplateView.as_view(template_name="about.html")),
-    path('contact', TemplateView.as_view(template_name="contact.html")),
-    path('login', TemplateView.as_view(template_name="login.html")),
+    path('', TemplateView.as_view(template_name="index.html"), name="home"),
+    path('about', TemplateView.as_view(template_name="about.html"), name="about"),
+    path('contact', TemplateView.as_view(template_name="contact.html"), name="contact"),
+    path('login', TemplateView.as_view(template_name="login.html"), name="login"),
 ]
