ebd928e17966f0035814b094b1d82be9128caabd
hartnetl <laura.hartnett@outlook.com>
2021-10-13 10:43:45 +0000

    Feat: Create FullRecipe view

--- recipeapp/views.py
+++ recipeapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Recipe
 
 
@@ -8,3 +8,20 @@ class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by('title')
     template_name = 'recipes.html'
     paginate_by = 8
+
+
+class FullRecipe(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.filter(approved=True).order_by('date_created')
+
+        return render(
+            request,
+            'view_recipe.html',
+            {
+                'recipe': recipe,
+                'comments': comments
+            },
+        )
