f23f504e2cb5ee66ca1a54e08af97cda1d580b15
hartnetl <laura.hartnett@outlook.com>
2021-10-14 11:17:51 +0000

    Feat: Create form for recipe model

---
+++ recipeapp/forms.py
@@ -0,0 +1,10 @@
+from.models import Recipe, Ingredients
+from django import forms
+
+
+class CreateRecipe(forms.ModelForm):
+
+    class Meta:
+        model = Recipe
+        fields = ('title', 'about', 'nutrition', 'servings', 'prep_time', 'cook_time', 'method', 'tags', 'status', 'featured_image', 'category',)
+
