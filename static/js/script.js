61ba7c7b339648da1e9fa83baea8d62ee9470210
hartnetl <laura.hartnett@outlook.com>
2021-10-28 01:28:27 +0000

    Feat: Add emailjs notifications to add/edit recipe and add comment button

--- static/js/script.js
+++ static/js/script.js
@@ -21,3 +21,26 @@ function sendMail(contactForm){
 function newAlert() {
     alert("If you have completed the form correctly you will receive a confirmation email shortly!")
 }
+
+function adminNotification() {
+    emailjs.send("outlook","lotrAPPROVE")
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+            contactForm.reset();
+            console.log("Form is reset");
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;
+}
+
+function submitAlert() {
+    alert("Thank you for your submission, admin will approve it shortly if suitable")
+}
+
+function editAlert() {
+    alert("You have edited a post successfully. Admin will overlook the changes to ensure they still follow community guidelines.")
+}
